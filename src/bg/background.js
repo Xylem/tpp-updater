015b4e394bba1f9afe709b7f5519f5f823a5fa00
Xylem <xylem.pl@gmail.com>
2014-03-02 13:50:18 +0100

    Version 0.0.5 - switched to custom server

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,35 +1,41 @@
-var LIVE_URL = "http://tpp.aninext.tv/feed.json";
+var LIVE_URL = 'http://54.194.157.89/feed';
 
 var NOTIFICATION_DURATION = 30000;
-var UPDATE_TIMEOUT = 5000;
+var UPDATE_TIMEOUT = 30000;
 
-var lastUpdateTime = 0;
+var lastUpdateId = 0;
 
-function readUpdates() {  
-    var url = LIVE_URL + "?i=" + (new Date()).getTime();
-    
-    jQuery.getJSON(url, function (data) {
-        if (data.updates) {
-            var newestUpdate = data.updates[0];
+function readUpdates() {     
+    jQuery.ajax({
+        dataType: 'json',
+        url: LIVE_URL,
+        headers: {
+            'last-id': lastUpdateId
+        },
+        success: function (data) {
+            if (data && data.data) {
+                var newestUpdate = data.data;
 
-            if (newestUpdate && newestUpdate.time > lastUpdateTime) {
-                lastUpdateTime = newestUpdate.time;
+                if (newestUpdate && newestUpdate.id !== lastUpdateId) {
+                    lastUpdateId = newestUpdate.id;
 
-                var message = jQuery(newestUpdate.title).text();
+                    var decoded = jQuery('<div/>').html(newestUpdate.body_html).text();
+                    var message = jQuery(decoded).text();
 
-                chrome.notifications.create("", {
-                    type: "basic",
-                    title: "Twitch Plays Pokemon", 
-                    message: message,
-                    iconUrl: "icons/icon48.png"
-                }, function (notificationId) {
-                    function clearNotification () {
-                        chrome.notifications.clear(notificationId, function () {});
-                    }
-                    
-                    setTimeout(clearNotification, NOTIFICATION_DURATION);
-                });
-            }
+                    chrome.notifications.create('', {
+                        type: 'basic',
+                        title: 'Twitch Plays Pokemon', 
+                        message: message,
+                        iconUrl: 'icons/icon48.png'
+                    }, function (notificationId) {
+                        function clearNotification () {
+                            chrome.notifications.clear(notificationId, function () {});
+                        }
+                        
+                        setTimeout(clearNotification, NOTIFICATION_DURATION);
+                    });
+                }
+            } 
         }
     });
     
